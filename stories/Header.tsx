bd58a31ec82d8f71f82a2d6116b1abc34c407d75
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-03 18:41:22 -0800

    Header Construction

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import './header.css';
 
-export const Header = () => (
-  <header>
-    <div className="storybook-header">
-      <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
-      </div>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const controls = useAnimation();
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    controls.start({
+      opacity: 1 - scrollY / 300, // Adjust the denominator to control the speed of the fade
+    });
+  }, [scrollY, controls]);
+
+  return (
+    <header>
+      <motion.div
+        className="storybook-header"
+        animate={controls}
+        initial={{ opacity: 1 }}
+      >
+        <div>
+          <h1>Stephen J. Lu</h1>
+        </div>
+      </motion.div>
+    </header>
+  );
+};
