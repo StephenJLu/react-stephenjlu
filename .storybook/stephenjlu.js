59c5ef1f1aa1499b84222beeadd3ca7f81ec071f
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-04 09:01:09 -0800

    added storybook

--- .storybook/stephenjlu.js
+++ .storybook/stephenjlu.js
@@ -1,9 +1,13 @@
-import { create } from '@storybook/theming';
+import { create } from '@storybook/theming/create';
+import '../styles/fonts.css';
 
-export default create({
-  base: 'dark',
+export default create({ 
+  base: 'light', 
   brandTitle: 'Stephen J. Lu',
   brandUrl: 'https://www.StephenJLu.com',
-  brandImage: '../static/images/SJL-logo.svg',
+  brandImage: 'https://www.stephenjlu.com/images/SJL-logo.svg',
   brandTarget: '_blank',
+
+  fontBase: '"Atlas Grotesk", sans-serif',
+  fontCode: 'monospace',
 });
---
+++ storybook-static/assets/menuBar.stories-Cq3dlKWD.js
@@ -0,0 +1,18 @@
+import{M as l}from"./MenuBar-B8ezlsS5.js";import{f as o}from"./index-B5ZI-g0m.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";const m={title:"Menu Bar",component:l,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},n={args:{items:[{label:"Home",onClick:o()},{label:"About",onClick:o()},{label:"Services",onClick:o()},{label:"Contact",onClick:o()}],backgroundColor:"#000"}};var e,a,r;n.parameters={...n.parameters,docs:{...(e=n.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      label: 'Home',
+      onClick: fn()
+    }, {
+      label: 'About',
+      onClick: fn()
+    }, {
+      label: 'Services',
+      onClick: fn()
+    }, {
+      label: 'Contact',
+      onClick: fn()
+    }],
+    backgroundColor: '#000'
+  }
+}`,...(r=(a=n.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const u=["Default"];export{n as Default,u as __namedExportsOrder,m as default};
