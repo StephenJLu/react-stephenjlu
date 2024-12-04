ba921117b825abea6431369dae84d6472da79f40
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-04 15:21:00 -0800

    Delete storybook-static directory

--- storybook-static/assets/preview-BEUI3sfV.js
+++
@@ -1,1 +0,0 @@
-const r={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},options:{storySort:(e,o)=>e.id===o.id?0:e.id.localeCompare(o.id,void 0,{numeric:!0})},controls:{expanded:!0,hideNoControlsWarning:!0,matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{r as default};
