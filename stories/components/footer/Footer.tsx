c332615f6c92faae0e0226bdb865a5ce57009e64
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-09 14:40:39 -0800

    Update Footer component to use delay from config and adjust import path for TextAnim

--- stories/components/footer/Footer.tsx
+++ stories/components/footer/Footer.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './footer.css';
 import config from '../../../config.json';
-import TextAnim from "../text-animation/TextAnim";
+import { TextAnim } from "../Components";
  
-export const Footer = () => {
-    
+const delay = config.delay;
+
+export const Footer = () => {   
   return (
     <div className="footer" data-bs-theme="dark">
       <div className="footer-content">
         <span className="date">
-          <TextAnim typeText={`© ${new Date().getFullYear()} ${config.name}. All rights reserved.`} />            
+          <TextAnim typeText={`© ${new Date().getFullYear()} ${config.name}. All rights reserved.`} delay={delay} />            
           </span>        
       </div>
     </div>
